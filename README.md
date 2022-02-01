4ee883e1f68a169e50afe0c3a2f8f31f675948a3
nathanielCherian <ngcherian201@gmail.com>
2022-01-31 20:47:44 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wordle-backtester
 Test optimal strategies for wordle 
+
+> "that what cannot be settled by experiment is not worth debating." 
+>> - Newton's flaming laser sword
+
+Word Lists taken from [cfreshman](https://gist.github.com/cfreshman).
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+string strategy() {
+    return "guess"
+}
+
+int main() {
+    cout << "Hello there!" << endl;
+
+    string ANSWER_WORD = "laugh";
+
+    for(int i=0; i<6; i++) {
+
+    }
+
+    return 0;
+}
